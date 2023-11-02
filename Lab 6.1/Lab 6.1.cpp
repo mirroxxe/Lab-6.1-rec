a3258a22d348b958e6ee8274023359aecdfd7fc4
Volodya <chuchman002@gmail.com>
2023-11-02 12:40:21 +0200

    Add project files.

---
+++ Lab 6.1/Lab 6.1.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+const int arraySize = 24;
+const int lowerLimit = -50;
+const int upperLimit = 15;
+
+void generateRandom(int arr[], int size, int lower, int upper) {
+    if (size == 0) {
+        return;
+    }
+
+    arr[size - 1] = rand() % (upper - lower + 1) + lower;
+    generateRandom(arr, size - 1, lower, upper);
+}
+
+void print(int arr[], int size) {
+    if (size == 0) {
+        std::cout << std::endl;
+        return;
+    }
+
+    std::cout << arr[size - 1] << " ";
+    print(arr, size - 1);
+}
+
+int countNegative(int arr[], int size) {
+    if (size == 0) {
+        return 0;
+    }
+
+    int count = countNegative(arr, size - 1);
+    if (arr[size - 1] < 0 || arr[size - 1] % 2 != 0) {
+        return count + 1;
+    }
+
+    return count;
+}
+
+int sumNegative(int arr[], int size) {
+    if (size == 0) {
+        return 0;
+    }
+
+    int sum = sumNegative(arr, size - 1);
+    if (arr[size - 1] < 0 || arr[size - 1] % 2 != 0) {
+        return sum + arr[size - 1];
+    }
+
+    return sum;
+}
+
+void replaceNegative(int arr[], int size) {
+    if (size == 0) {
+        return;
+    }
+
+    if (arr[size - 1] < 0 || arr[size - 1] % 2 != 0) {
+        arr[size - 1] = 0;
+    }
+
+    replaceNegative(arr, size - 1);
+}
+
+int main() {
+    int arr[arraySize];
+    srand(static_cast<unsigned>(time(0)));
+
+    generateRandom(arr, arraySize, lowerLimit, upperLimit);
+    std::cout << "Generated array: ";
+    print(arr, arraySize);
+
+    int count = countNegative(arr, arraySize);
+    int sum = sumNegative(arr, arraySize);
+
+    std::cout << "Count of negative: " << count << std::endl;
+    std::cout << "Sum of negative: " << sum << std::endl;
+
+    replaceNegative(arr, arraySize);
+    std::cout << "Modified array: ";
+    print(arr, arraySize);
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 6.1/Lab 6.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+
+		TEST_METHOD(TestGenerateRandom)
+		{
+			int arr[arraySize];
+			generateRandom(arr, arraySize, lowerLimit, upperLimit);
+
+			for (int i = 0; i < arraySize; i++) {
+				Assert::IsTrue(arr[i] >= lowerLimit && arr[i] <= upperLimit);
+			}
+		}
+
+
+		TEST_METHOD(TestReplaceNegative)
+		{
+			int arr[] = { -3, 4, -5, 7, -2, 10 };
+			int size = sizeof(arr) / sizeof(arr[0]);
+
+			replaceNegative(arr, size);
+
+			for (int i = 0; i < size; i++) {
+				if (arr[i] < 0 || arr[i] % 2 != 0) {
+					Assert::AreEqual(0, arr[i]);
+				}
+			}
+		}
+	};
+}
